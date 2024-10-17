b71811d12daf6b2b2b75f3e3f299b1ff0c89a2b9
Patrick <wysspatrick2@gmail.com>
2024-10-17 19:56:08 +0200

    setup

---
+++ matrix/include/matrix.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstddef>
+template <size_t Rows, size_t Cols, typename T = double>
+class Matrix
+{
+   public:
+   private:
+};
---
+++ tests/unit/matrix_creation_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include <matrix.hpp>
+
+TEST(dummyTest, dummy)
+{
+    EXPECT_TRUE(true);
+}
